c3e4a532629c606d9f7e09331ade100cfa891082
Micaela <mleivaachaval@ufl.edu>
2024-03-31 22:01:57 -0400

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
-const card = document.getElementById("card");
+const hidden = document.getElementById("hidden");
+const seen = document.getElementById("seen");
+const readMore = document.getElementById("readmore");
+const readLess = document.getElementById("readless");
 
-card.addEventListener("click", flipCard);
+hidden.style.display = "none";
+readLess.style.display = "none";
 
-function flipCard() {
-    card.classList.toggle("flipCard")
+readMore.onclick = () => {
+    hidden.style.display = "block";
+    readLess.style.display = seen.style.display = "block";
+    readMore.style.display = "none";
+    seen.style.display = "none";
+};
+
+readLess.onclick = () => {
+    hidden.style.display = readLess.style.display = "none";
+    readMore.style.display = seen.style.display = "block";
 };
